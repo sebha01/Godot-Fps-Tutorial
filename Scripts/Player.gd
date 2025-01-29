64d52db28b9bcc11602fe73c87fceccb1ac38a03
sebha01 <30077548@students.southwales.ac.uk>
2025-01-29 18:55:24 +0000

    Mouse movement for camera rotation implemented

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,8 +3,20 @@ extends CharacterBody3D
 
 const SPEED = 10.0
 const JUMP_VELOCITY = 6.5
+const SENSITIVITY = 0.003
 
 var gravity = 9.8
+
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)	
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
 
 func _physics_process(delta):
 	# Add the gravity.
